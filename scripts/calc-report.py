2ba0b6249257a4ced5926ad02f6a3dc4e345a770
vladimir.dolzhenko <vladimir.dolzhenko@localhost>
2012-04-28 22:55:15 +0400

    (*) reworked start and report scripts

--- scripts/calc-report.py
+++ scripts/calc-report.py
@@ -11,9 +11,18 @@ if __name__ == '__main__':
 	
 	i = 0
 
+	width = 1200
+	height = 600
+
 	reader = open(sys.argv[1], "r")
-	writer = open(sys.argv[1] + '.csv', "w")
-	writer2 = open(sys.argv[1] + '.js', "w")
+	jsname = '%s.js' % sys.argv[1]
+	jswriter = open(jsname, "w")
+	htmlwriter = open(sys.argv[1] + '.html', "w")
+
+	charts = []	
+
+	colors = ["'#F00'", "'#080'", "'#00009c'", "'orange'", "'purple'", "'grey'", "'cyan'", "'black'"]
+
 
 	name = ''
 	threads = ''
@@ -41,13 +50,13 @@ if __name__ == '__main__':
 			threadsMap[int(threads)] = True
 			messagesMap[int(messages)] = True
 
-		if idx == 1 :
+		elif idx == 1 :
 			time = float(a[3])
 			key = name + '_' + threads + '_' + messages
 			if key not in timeMap:
-				timeMap[key] = 0.0	
-			timeMap[key] = timeMap[key] + time/3
-		if idx == 2 :	
+				timeMap[key] = []
+			timeMap[key].append(time)
+		elif idx == 2 :	
 			if len(line) == 0:
 				gc = 0.0
 			else :
@@ -55,8 +64,8 @@ if __name__ == '__main__':
 			
 			key = name + '_' + threads + '_' + messages
 			if key not in gcMap:
-				gcMap[key] = 0.0	
-			gcMap[key] = gcMap[key] + gc/3
+				gcMap[key] = []
+			gcMap[key].append(gc)
 
 		i = i + 1
 	
@@ -66,7 +75,8 @@ if __name__ == '__main__':
 	for name in sortedKeys(loggerMap):
 		v.append(name + " throughput")	
 		v.append(name + " gc")	
-	writer.write('%s\n' % ','.join(v))
+
+	jswriter.write('function drawChart(){ \n')
 
 	for threads in sortedKeys(threadsMap):
 		v = []
@@ -77,10 +87,11 @@ if __name__ == '__main__':
 	
 			for name in sortedKeys(loggerMap):
 				key = name + '_' + str(threads) + '_' + str(messages)
-				v2.append('%.2f' % (float(messages) / timeMap[key]))
+				avg = sum(timeMap[key])/len(timeMap[key])
+				v2.append('%.2f' % (float(messages) / avg))
 				# into ms
-				v2.append('%.2f' % (1000 * gcMap[key]))
-			writer.write('%s\n' % ','.join(v2))
+				avg = sum(gcMap[key])/len(gcMap[key])
+				v2.append('%.2f' % (1000 * avg))
 
 	for threads in sortedKeys(threadsMap):
 		t = str(threads)
@@ -97,7 +108,8 @@ if __name__ == '__main__':
 	
 			for name in sortedKeys(loggerMap):
 				key = name + '_' + str(threads) + '_' + str(messages)
-				v2.append('%.2f' % (float(messages) / timeMap[key]))
+				avg = sum(timeMap[key])/len(timeMap[key])
+				v2.append('%.2f' % (float(messages) / avg))
 				# into ms
 				#v2.append('%.2f' % (1000 * gcMap[key]))
 				vc.append(name)
@@ -112,25 +124,29 @@ if __name__ == '__main__':
 			logger_names = "%s\n\
 		data.addColumn('%s', '%s');" % (logger_names, type, vc[i])
 
+		chartname = 'throughput_%s_chart' % t
 
-		writer2.write("/********* %s %s **************/ \n\
+		charts.append(chartname)
+
+		jswriter.write("\t/********* %s %s **************/ \n\
 		//throughput	\n\
 		data = new google.visualization.DataTable();	\n\
-		%s \n\
+			%s \n\
 		data.addRows([	\n\
-%s				\n\
+			%s\n\
 		]);				\n\
 	\n\
-        chart = new google.visualization.LineChart(document.getElementById('throughput_%s_chart')); \n\
+        chart = new google.visualization.LineChart(document.getElementById('%s')); \n\
         chart.draw(data, \n\
 		{		\n\
-          width: 600, height: 250, \n\
+          width: %d, height: %d, \n\
           title: 'Throughput, %s %s', \n\
           hAxis: {title: 'number of messages',  titleTextStyle: {color: '#000'}, logScale: true}, \n\
           vAxis: {title: 'messages / ms', gridlines: {color: '#ccc', count: 8}}, \n\
-          legend: {position: 'right', textStyle: {color: 'black', fontSize: 10}} \n\
+          legend: {position: 'right', textStyle: {color: 'black', fontSize: 10}}, \n\
+		  colors: [%s]\n\
         });\n\
-		" % (ts, tname, logger_names, ',\n'.join(v), t, ts, tname))
+		" % (ts, tname, logger_names, ',\n\t\t\t'.join(v), chartname, width, height, ts, tname, ','.join(colors)))
 		
 
 		v = []
@@ -143,7 +159,8 @@ if __name__ == '__main__':
 				key = name + '_' + str(threads) + '_' + str(messages)
 				#v2.append('%.2f' % (float(messages) / timeMap[key]))
 				# into ms
-				v2.append('%.2f' % (1000 * gcMap[key]))
+				avg = sum(gcMap[key])/len(gcMap[key])
+				v2.append('%.2f' % (1000 * avg))
 				vc.append(name)
 			v.append('[' + ( ','.join(v2)) + ']')	
 
@@ -154,25 +171,45 @@ if __name__ == '__main__':
 				type = 'string'	
 			logger_names = "%s\n\
 		data.addColumn('%s', '%s');" % (logger_names, type, vc[i])
-	
+		
 
-		writer2.write("//gc \n\
+		chartname = 'gc_%s_chart' % t
+
+		charts.append(chartname)
+
+
+		jswriter.write("//gc \n\
 		data = new google.visualization.DataTable(); \n\
 		%s\n\
 		data.addRows([ \n\
-%s \n\
+			%s \n\
 		]); \n\
 \n\
-		chart = new google.visualization.LineChart(document.getElementById('gc_%s_chart'));\n\
+		chart = new google.visualization.LineChart(document.getElementById('%s'));\n\
         chart.draw(data, \n\
         {\n\
-          width: 600, height: 250,\n\
+          width: %d, height: %d,\n\
           title: 'Total stop the world, %s %s',\n\
           hAxis: {title: 'number of messages',  titleTextStyle: {color: '#000'}, logScale: true},\n\
           vAxis: {title: 'ms', gridlines: {color: '#ccc', count: 8}},\n\
-          legend: {position: 'right', textStyle: {color: 'black', fontSize: 10}}\n\
-        });\n" % (logger_names, ',\n'.join(v), t, ts, tname ))
+          legend: {position: 'right', textStyle: {color: 'black', fontSize: 10}},\n\
+		  colors: [%s]\n\
+        });\n" % (logger_names, ',\n\t\t\t'.join(v), chartname, width, height, ts, tname, ','.join(colors)))
 
-	writer.close()	
-	writer2.close()
+	jswriter.write('}\n')	
 
+	htmlwriter.write('<html>\n\
+<body>\n\
+<div id="chart"></div>\n\
+%s\n\
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>\n\
+<script type="text/javascript" src="%s"></script>\n\
+<script type="text/javascript">\n\
+      google.load("visualization", "1", {packages:["imagelinechart", "imagechart", "corechart"]});\n\
+      google.setOnLoadCallback(drawChart);\n\
+</script>\n\
+</body> \n\
+</html>\n' % ( "\n".join(map(lambda c: '<div id="%s"></div>' % c, charts)), jsname ) )
+
+	jswriter.close()
+	htmlwriter.close()
