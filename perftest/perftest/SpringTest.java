a843ec42fff9da70cae71b00f7ba51f1057311b3
dolzvla <dolzvla@localhost>
2012-07-23 17:47:10 +0400

    test enh

--- perftest/perftest/SpringTest.java
+++ perftest/perftest/SpringTest.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * SpringTest
- * 
+ *
  * @author Vladimir Dolzhenko, vladimir.dolzhenko@gmail.com
  */
 public class SpringTest {
@@ -16,15 +16,19 @@ public class SpringTest {
 	public static void main(String[] args) {
 	    final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:app-context.xml");
 	    ctx.start();
-	    
+
 	    final Logger logger = LogFactory.getLog("com.db.fxpricing.Logger");
-	    
+
 	    logger.info().append("test1").commit();
-	    
+
+	    logger.debug().append("testD").commit();
+
+	    logger.error().append("testE").commit();
+
 	    logger.info().append("test2").commit();
-	    
+
 	    logger.info().append("test3").commit();
-	    
+
 	    ctx.stop();
     }
 }
